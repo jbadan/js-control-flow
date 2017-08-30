277ee004271d2c067f63b1e4fd1d475895a52886
jbadan <jenna.badanowski@gmail.com>
2017-08-30 16:55:29 -0700

    added celcius converter

--- js/tempConvert.js
+++ js/tempConvert.js
@@ -6,7 +6,7 @@
 // T(°C) = (T(°F) - 32) × 5/9
 // var fahren
 
-function convertTemp(fahren){
+function convertTempToC(fahren){
   var celcius = Math.floor((fahren - 32)*(5/9));
   if(fahren === 1){
     console.log(fahren+' degree Fahrenheit is '+celcius+' degrees Celsius.');
@@ -17,5 +17,17 @@ function convertTemp(fahren){
     console.log(fahren+' degrees Fahrenheit is '+celcius+' degrees Celsius.');
   }
 };
+function convertTempToF(celcius2){
+  var fahren2 = Math.floor((celcius2 - 32)*(5/9));
+  if(fahren2 === 1){
+    console.log(celcius2+' degrees Celcius is '+fahren2+' degree Fahrenheit.');
+  }else if(celcius2 === 1){
+    console.log(celcius2+' degree Celcius is '+fahren2+' degrees Fahrenheit.');
+  }
+  else{
+    console.log(celcius2+' degrees Celcius is '+fahren2+' degrees Fahrenheit.');
+  }
+};
 
-convertTemp(70);
+convertTempToC(70);
+convertTempToF(33);
